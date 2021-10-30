8b2f3711c96a302c3a360cc8c90be71a4e299188
ori davidi <oridavidi100@gmail.com>
2021-10-30 14:40:03 +0300

    ff

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+  
+  
+///
+ 
+
+  'use strict';
+import _ from 'lodash';
+import './style.css';
+
+import Controller from './controller';
+
+const app = new Controller();
+
+app.run();
+  app.run();
