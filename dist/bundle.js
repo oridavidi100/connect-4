be64b40925707e48615efe1f46c51df38b25c5a9
ori davidi <oridavidi100@gmail.com>
2022-01-17 12:06:16 +0200

    webpack

--- dist/bundle.js
+++ dist/bundle.js
@@ -7,8 +7,26 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module '@babel/core'\\nRequire stack:\\n- C:\\\\Users\\\\orida\\\\OneDrive\\\\Desktop\\\\ori\\\\Projects\\\\GitHub\\\\4Line\\\\node_modules\\\\babel-loader\\\\lib\\\\index.js\\n- C:\\\\Users\\\\orida\\\\OneDrive\\\\Desktop\\\\ori\\\\Projects\\\\GitHub\\\\4Line\\\\node_modules\\\\loader-runner\\\\lib\\\\loadLoader.js\\n- C:\\\\Users\\\\orida\\\\OneDrive\\\\Desktop\\\\ori\\\\Projects\\\\GitHub\\\\4Line\\\\node_modules\\\\loader-runner\\\\lib\\\\LoaderRunner.js\\n- C:\\\\Users\\\\orida\\\\OneDrive\\\\Desktop\\\\ori\\\\Projects\\\\GitHub\\\\4Line\\\\node_modules\\\\webpack\\\\lib\\\\NormalModuleFactory.js\\n- C:\\\\Users\\\\orida\\\\OneDrive\\\\Desktop\\\\ori\\\\Projects\\\\GitHub\\\\4Line\\\\node_modules\\\\webpack\\\\lib\\\\Compiler.js\\n- C:\\\\Users\\\\orida\\\\OneDrive\\\\Desktop\\\\ori\\\\Projects\\\\GitHub\\\\4Line\\\\node_modules\\\\webpack\\\\lib\\\\webpack.js\\n- C:\\\\Users\\\\orida\\\\OneDrive\\\\Desktop\\\\ori\\\\Projects\\\\GitHub\\\\4Line\\\\node_modules\\\\webpack\\\\lib\\\\index.js\\n- C:\\\\Users\\\\orida\\\\OneDrive\\\\Desktop\\\\ori\\\\Projects\\\\GitHub\\\\4Line\\\\node_modules\\\\webpack-cli\\\\lib\\\\webpack-cli.js\\n- C:\\\\Users\\\\orida\\\\OneDrive\\\\Desktop\\\\ori\\\\Projects\\\\GitHub\\\\4Line\\\\node_modules\\\\webpack-cli\\\\lib\\\\bootstrap.js\\n- C:\\\\Users\\\\orida\\\\OneDrive\\\\Desktop\\\\ori\\\\Projects\\\\GitHub\\\\4Line\\\\node_modules\\\\webpack-cli\\\\bin\\\\cli.js\\n- C:\\\\Users\\\\orida\\\\OneDrive\\\\Desktop\\\\ori\\\\Projects\\\\GitHub\\\\4Line\\\\node_modules\\\\webpack\\\\bin\\\\webpack.js\\n babel-loader@8 requires Babel 7.x (the package '@babel/core'). If you'd like to use Babel 6.x ('babel-core'), you should install 'babel-loader@7'.\\n    at Function.Module._resolveFilename (internal/modules/cjs/loader.js:902:15)\\n    at Function.Module._load (internal/modules/cjs/loader.js:746:27)\\n    at Module.require (internal/modules/cjs/loader.js:974:19)\\n    at require (internal/modules/cjs/helpers.js:93:18)\\n    at Object.<anonymous> (C:\\\\Users\\\\orida\\\\OneDrive\\\\Desktop\\\\ori\\\\Projects\\\\GitHub\\\\4Line\\\\node_modules\\\\babel-loader\\\\lib\\\\index.js:10:11)\\n    at Module._compile (internal/modules/cjs/loader.js:1085:14)\\n    at Object.Module._extensions..js (internal/modules/cjs/loader.js:1114:10)\\n    at Module.load (internal/modules/cjs/loader.js:950:32)\\n    at Function.Module._load (internal/modules/cjs/loader.js:790:12)\\n    at Module.require (internal/modules/cjs/loader.js:974:19)\");\n\n//# sourceURL=webpack://src/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
 /******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
 /******/ })()
 ;
